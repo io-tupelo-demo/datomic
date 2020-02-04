dba0bc3085c523e6d88e4c69c0bf8bd33e3afc09
Alan Thompson <cloojure@gmail.com>
2020-02-04 09:02:01 -0800

    misc

--- project.clj
+++ project.clj
@@ -1,15 +1,18 @@
-(defproject io.tupelo/datomic-demo "19.12.01"
+(defproject io.tupelo.demo/datomic "20.01.19"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [io.tupelo/datomic "20.01.19" ]
+  :dependencies [; libs are in groups or priority order, since in Lein "first one wins"
+
+                 ; High-priority libs
+                 [com.datomic/datomic-free "0.9.5703.21"]
                  [org.clojure/clojure "1.10.1"]
+
+                 ; Normal-priority libs
+                 [io.tupelo/datomic "20.01.19" ]
                  [prismatic/schema "1.1.12"]
                  [tupelo "0.9.188"]
                  ]
-  :resource-paths ["resources/"
-                   "resources/datomic-free-0.9.5661-everything.jar"
-                  ]
+  :resource-paths ["resources/" ]
   :global-vars { *warn-on-reflection* false }
 
   :update :daily ;  :always
